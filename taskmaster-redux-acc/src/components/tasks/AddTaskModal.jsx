137cb449988d47fa86cf0d15cabb7d361f2c2500
isalim079 <salimreza1025@gmail.com>
2024-02-18 17:42:54 +0600

    AddTask modal added and Modal data passes dynamically

---
+++ taskmaster-redux-acc/src/components/tasks/AddTaskModal.jsx
@@ -0,0 +1,14 @@
+import Modal from "../ui/Modal";
+
+
+const AddTaskModal = ({isOpen, setIsOpen}) => {
+    return (
+        <div>
+              <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Programming Hero"  > 
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laboriosam, impedit sequi quae quisquam sunt amet obcaecati earum odit atque quam. Quia earum rem, amet omnis tempora perferendis consectetur ipsum.
+              </ Modal>
+        </div>
+    );
+};
+
+export default AddTaskModal;
