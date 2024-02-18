6ddc4feec40a46ca133184d2718cacd72399b238
isalim079 <salimreza1025@gmail.com>
2024-02-18 09:36:57 +0600

    logger next function created

--- redux_test/src/redux/middlewares/Logger.js
+++ redux_test/src/redux/middlewares/Logger.js
@@ -1,6 +1,9 @@
 const logger = (store) => (next) => (action) => {
     const currentState = store.getState()
-    console.log(currentState);
+    console.log("currentState => ", currentState);
+    console.log("Action dispatched => ", action);
+    next(action)
+    console.log("Updated State => ", store.getState());
 }
 
 export default logger
