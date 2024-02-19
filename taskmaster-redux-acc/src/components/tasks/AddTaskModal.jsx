2a5bcbeadf2954ce4dd1e985d0a3997639d4dea4
isalim079 <salimreza1025@gmail.com>
2024-02-19 16:51:46 +0600

    input form added in addTaskModal

--- taskmaster-redux-acc/src/components/tasks/AddTaskModal.jsx
+++ taskmaster-redux-acc/src/components/tasks/AddTaskModal.jsx
@@ -1,13 +1,24 @@
 import Modal from "../ui/Modal";
+import {useForm} from "react-hook-form"
 
 
 const AddTaskModal = ({isOpen, setIsOpen}) => {
+
+  const {register, handleSubmit} = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
     return (
         <div>
            
               <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Programming Hero"  > 
                 
-                
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <input type="text" {...register('name')} />
+                  <button type="submit">Submit</button>
+                </form>
 
               </ Modal>
         </div>
