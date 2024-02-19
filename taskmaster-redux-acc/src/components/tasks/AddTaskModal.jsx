2e72ea9569421faa6c7acfaa1d6be5cc51774f3f
isalim079 <salimreza1025@gmail.com>
2024-02-19 09:46:02 +0600

    tailwind form plugin added

--- taskmaster-redux-acc/src/components/tasks/AddTaskModal.jsx
+++ taskmaster-redux-acc/src/components/tasks/AddTaskModal.jsx
@@ -4,9 +4,11 @@ import Modal from "../ui/Modal";
 const AddTaskModal = ({isOpen, setIsOpen}) => {
     return (
         <div>
-          
+           
               <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Programming Hero"  > 
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laboriosam, impedit sequi quae quisquam sunt amet obcaecati earum odit atque quam. Quia earum rem, amet omnis tempora perferendis consectetur ipsum.
+                
+                
+
               </ Modal>
         </div>
     );
