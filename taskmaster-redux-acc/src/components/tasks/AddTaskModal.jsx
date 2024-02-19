b675b0c81079d3b642482e8f742eadc6b2416466
isalim079 <salimreza1025@gmail.com>
2024-02-19 19:58:19 +0600

    addTaskModal form created

--- taskmaster-redux-acc/src/components/tasks/AddTaskModal.jsx
+++ taskmaster-redux-acc/src/components/tasks/AddTaskModal.jsx
@@ -1,13 +1,20 @@
+/* eslint-disable no-unused-vars */
 import Modal from "../ui/Modal";
 import {useForm} from "react-hook-form"
 
 
 const AddTaskModal = ({isOpen, setIsOpen}) => {
 
-  const {register, handleSubmit} = useForm()
+  const {register, handleSubmit, reset} = useForm()
+
+  const onCancel = () => {
+    reset ()
+    setIsOpen(false)
+  }
 
   const onSubmit = (data) => {
     console.log(data);
+    onCancel()
   }
 
     return (
@@ -16,8 +23,42 @@ const AddTaskModal = ({isOpen, setIsOpen}) => {
               <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Programming Hero"  > 
                 
                 <form onSubmit={handleSubmit(onSubmit)}>
-                  <input type="text" {...register('name')} />
-                  <button type="submit">Submit</button>
+
+                 <div className="flex mt-5 flex-col gap-1">
+                  <label htmlFor="title" className="mb-2">Title</label>
+                  <input type="text" className="w-full" id="title" {...register('title')} />
+                 </div>
+
+                 <div className="flex mt-5 flex-col gap-1">
+                  <label htmlFor="title" className="mb-2">Description</label>
+                  <input type="text" className="w-full" id="description" {...register('description')} />
+                 </div>
+
+                 <div className="flex mt-5 flex-col gap-1">
+                  <label htmlFor="title" className="mb-2">Deadline</label>
+                  <input type="date" className="w-full" id="date" {...register('date')} />
+                 </div>
+
+                 <div className="flex mt-5 flex-col gap-1">
+                  <label htmlFor="title" className="mb-2">Assign To</label>
+                  <select  className="w-full" id="assignedTo" {...register('assignedTo')} > 
+                  
+                  <option value="Mir Hussain">Mir Hussain</option>
+                 <option value="Mezba Abedin">Mezba Abedin</option>
+                 <option value="Nahid Hasan">Nahid Hasan</option>
+                 <option value="Rahatul Islam">Rahatul Islam</option>
+                 <option value="Mir Hussain">Mir Hussain</option>
+                 <option value="Mezba Abedin">Mezba Abedin</option>
+                 <option value="Nahid Hasan">Nahid Hasan</option>
+                 <option value="Rahatul Islam">Rahatul Islam</option>
+
+                  </select>
+                 </div> 
+
+                  <div className="flex justify-end gap-3 mt-5">
+                  <button onClick={() => onCancel()} className="bg-red-600 rounded-md  px-3 py-2 text-white">Cancel</button>
+                  <button className="bg-green-600 rounded-md  px-3 py-2 text-white " type="submit">Submit</button>
+                  </div>
                 </form>
 
               </ Modal>
