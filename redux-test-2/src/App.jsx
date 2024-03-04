b835d492208cc649be93109d2200ba3ea67dc7aa
isalim079 <salimreza1025@gmail.com>
2024-03-04 17:59:55 +0600

    redux second practice file created

---
+++ redux-test-2/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+
+import Home from "./pages/Home";
+
+
+function App() {
+    return (
+        <>
+         <Home />
+        </>
+    );
+}
+
+export default App;
